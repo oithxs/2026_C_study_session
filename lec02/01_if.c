75f919951ae1244d0fe39c9e005e60afe07e990d
soshi-1982 <shuangshigongtian@gmail.com>
2026-04-29 20:31:29 +0900

    if文

---
+++ lec02/01_if.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(void){
+    int a = 10,b;
+    printf("Enter an integer: ");
+    scanf("%d", &b);
+    if(a > b){
+        printf("a is greater than b\n");
+    } else {
+        printf("a is not greater than b\n");
+    }
+    return 0;
+}
