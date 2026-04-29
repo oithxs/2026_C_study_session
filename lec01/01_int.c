fab3945b520ff31dae0685fcfc45144e677c40be
soshi-1982 <shuangshigongtian@gmail.com>
2026-04-29 18:28:07 +0900

    四則演算

--- lec01/01_int.c
+++ lec01/01_int.c
@@ -2,7 +2,13 @@
 int main(void){
     int a = 10;
     int b = 20;
-    int sum = a + b;
-    printf("a + b = %d\n", sum);
+    int sub = a - b;
+    int mul = a * b;
+    int div = a / b;
+    int mod = a % b;
+    printf("a - b = %d\n", sub);
+    printf("a * b = %d\n", mul);
+    printf("a / b = %d\n", div);
+    printf("a %% b = %d\n", mod);
     return 0;
 }
