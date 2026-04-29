11293a11609664245325c8b78ee26a01f82eb968
soshi-1982 <shuangshigongtian@gmail.com>
2026-04-29 18:44:54 +0900

    double型の四則演算

--- lec01/03_double.c
+++ lec01/03_double.c
@@ -2,7 +2,13 @@
 int main(void){
     double a = 3.14;
     double b = 2.71;
-    printf("a = %lf\n", a);
-    printf("b = %lf\n", b);
+    double sum = a + b;
+    double sub = a - b;
+    double mul = a * b;
+    double div = a / b;
+    printf("a + b = %lf\n", sum);
+    printf("a - b = %lf\n", sub);
+    printf("a * b = %lf\n", mul);
+    printf("a / b = %lf\n", div);
     return 0;
 }
