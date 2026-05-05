4ee63a0e8e97cf474a716cb9057e52110067562f
soshi-1982 <shuangshigongtian@gmail.com>
2026-05-05 18:56:35 +0900

    論理演算子||

---
+++ lec02/02_logical_op.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void){
+    int a,b;
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
+    if(a > 0 || b > 0){
+        printf("At least one of the numbers is positive\n");
+    } else {
+        printf("Neither number is positive\n");
+    }
+    
+    return 0;
+}
