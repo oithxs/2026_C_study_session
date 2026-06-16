d07dc1db2ea422b07dc3950d1a8af00470c8216d
soshi-1982 <shuangshigongtian@gmail.com>
2026-06-17 07:19:59 +0900

    break文

--- lec02/06_infinite_loop.c
+++ lec02/06_infinite_loop.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
 int main(void){
-        
+    int a;
     while(1){
-        printf("This is an infinite loop.\n");
+        printf("please enter a positive integer: ");
+        scanf("%d", &a);
+        printf("You entered: %d\n", a);
+        if(!(a > 0)){
+            break;
+        }
     }
 
     return 0;
