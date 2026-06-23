07ddcb9f43e82ccf578d8f057ecd98793aa38561
soshi-1982 <shuangshigongtian@gmail.com>
2026-06-23 19:17:47 +0900

    配列

---
+++ lec03/01_array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void) {
+    int array[5];
+    array[0] = 1;
+    array[1] = 2;
+    array[2] = 3;
+    array[3] = 4;
+    array[4] = 5;
+    printf("array[0] = %d\n", array[0]);
+    printf("array[1] = %d\n", array[1]);
+    printf("array[2] = %d\n", array[2]);
+    printf("array[3] = %d\n", array[3]);
+    printf("array[4] = %d\n", array[4]);
+    return 0;
+}
