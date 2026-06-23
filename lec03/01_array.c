0930287a38c72f4286d20180390bdaa722ef6495
soshi-1982 <shuangshigongtian@gmail.com>
2026-06-24 07:38:23 +0900

    配列2

--- lec03/01_array.c
+++ lec03/01_array.c
@@ -1,16 +1,10 @@
 #include<stdio.h>
 
 int main(void) {
-    int array[5];
-    array[0] = 1;
-    array[1] = 2;
-    array[2] = 3;
-    array[3] = 4;
-    array[4] = 5;
-    printf("array[0] = %d\n", array[0]);
-    printf("array[1] = %d\n", array[1]);
-    printf("array[2] = %d\n", array[2]);
-    printf("array[3] = %d\n", array[3]);
-    printf("array[4] = %d\n", array[4]);
+    int array[5] = {1, 2, 3, 4, 5};
+    int i;
+    for (i = 0; i < 5; i++) {
+        printf("array[%d] = %d\n", i, array[i]);
+    }
     return 0;
 }
