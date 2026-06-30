1f088792b9451c40b8a5337aefe6a8fc5ee00d38
soshi-1982 <shuangshigongtian@gmail.com>
2026-06-30 18:13:04 +0900

    戻り値

---
+++ lec04/03_return.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+
+int getInteger(void){
+    int input;
+    printf("Please enter a positive integer: ");
+    scanf("%d", &input);
+    return input;
+}
+char getOp(void){
+    char op;
+    printf("Please enter a operator: ");
+    scanf(" %c", &op);
+    return op;
+}
+
+int alu (int a, int b, char op){
+    if(op == '+'){
+        return a + b;
+    }else if(op == '-'){
+        return a - b;
+    }else if(op == '*'){
+        return a * b;
+    }else if(op == '/') {
+        return a / b;
+    }
+    printf("ERROR: invalid operator\n");
+    return -1;
+}
+
+int main(void){
+    int num1, num2,sum;
+    char op;
+    printf("-----------\n");
+    printf("start\n");
+
+    num1 = getInteger();
+    num2 = getInteger();
+    op = getOp();
+    sum = alu(num1, num2, op);
+    printf("%d %c %d = %d\n", num1, op, num2, sum);
+    
+    printf("end\n");
+    printf("-----------\n");
+    return 0;
+}
