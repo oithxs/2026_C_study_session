0d26b3f6900c30a4ad11748eab5450abcc2d46c6
soshi-1982 <shuangshigongtian@gmail.com>
2026-06-30 19:38:13 +0900

    プログラム引数

---
+++ lec04/04_program_ar.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main (int argc, char*argv[]){
+    int i;
+    printf("argc = %d\n", argc);
+    printf("argv = {\n");
+    for(i = 0; i < argc; i++)
+        printf("%s\n", argv[i]);
+    printf("}\n");
+    return 0;
+}
